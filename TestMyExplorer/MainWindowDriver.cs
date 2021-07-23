7ce482ace593c3d9b852402ce1e0967d59f2b75a
FourHyphen <FourHyphen.4@gmail.com>
2021-07-23 14:46:23 +0900

    テストに必要な環境を整備

---
+++ TestMyExplorer/MainWindowDriver.cs
@@ -0,0 +1,25 @@
+﻿using Codeer.Friendly.Windows.Grasp;
+using RM.Friendly.WPFStandardControls;
+using System;
+using System.Collections.Generic;
+
+namespace TestMyExplorer
+{
+    class MainWindowDriver
+    {
+        private dynamic MainWindow { get; }
+
+        private IWPFDependencyObjectCollection<System.Windows.DependencyObject> Tree { get; set; }
+
+        public MainWindowDriver(dynamic mainWindow)
+        {
+            MainWindow = mainWindow;
+            Tree = new WindowControl(mainWindow).LogicalTree();
+        }
+
+        private void UpdateNowMainWindowStatus()
+        {
+            Tree = new WindowControl(MainWindow).LogicalTree();    // 現在の画面状況を取得
+        }
+    }
+}
---
+++ TestMyExplorer/TestFeature.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMyExplorer
+{
+    [TestClass]
+    public class TestFeature
+    {
+        // 必要なパッケージ
+        //  -> Codeer.Friendly
+        //  -> Codeer.Friendly.Windows         -> WindowsAppFriend()
+        //  -> Codeer.Friendly.Windows.Grasp   -> WindowControl()
+        //  -> RM.Friendly.WPFStandardControls -> 各種WPFコントロールを取得するために必要
+        // 必要な作業
+        //  -> MyExplorer プロジェクトを参照に追加
+
+        private string AttachExeName = "MyExplorer.exe";
+        private WindowsAppFriend TestApp;
+        private Process TestProcess;
+        private dynamic MainWindow;
+        private MainWindowDriver MainWindowDriver;
+
+        private string BeforeEnvironment { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            // MainWindowプロセスにattach
+            string exePath = System.IO.Path.GetFullPath(AttachExeName);
+            TestApp = new WindowsAppFriend(Process.Start(exePath));
+            TestProcess = Process.GetProcessById(TestApp.ProcessId);
+            MainWindow = TestApp.Type("System.Windows.Application").Current.MainWindow;
+
+            MainWindowDriver = new MainWindowDriver(MainWindow);
+
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            TestApp.Dispose();
+            TestProcess.CloseMainWindow();
+            TestProcess.Dispose();
+
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+    }
+}
