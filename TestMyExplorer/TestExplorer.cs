628877d4fb841c17c6303868dd98ab3a79e1628e
FourHyphen <FourHyphen.4@gmail.com>
2021-07-27 07:46:10 +0900

    Explorerクラスのインスタンス生成テストを追加

---
+++ TestMyExplorer/TestExplorer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyExplorer;
+
+namespace TestMyExplorer
+{
+    [TestClass]
+    public class TestExplorer
+    {
+        private string BeforeEnvironment { get; set; }
+
+        [TestInitialize]
+        public void Init()
+        {
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+
+        [TestMethod]
+        public void CreateExplorer()
+        {
+            string folderPath = Common.GetFilePathOfDependentEnvironment(@"./TestData/Folder1");
+            Explorer explorer = new Explorer(folderPath, 0);
+            Assert.IsTrue(explorer.Data.FolderPath.Contains(@"TestData\Folder1"));
+
+            Assert.IsTrue(GetPrivateField(explorer, "FolderArea").Name == "FolderArea0");
+            Assert.IsTrue(GetPrivateField(explorer, "Folder").Name == "Folder0");
+            Assert.IsTrue(GetPrivateField(explorer, "FolderFileList").Name == "FileList0");
+
+            Assert.IsTrue(System.IO.Path.GetFileName(explorer.Data.FileList[0]) == "folder01");
+            Assert.IsTrue(System.IO.Path.GetFileName(explorer.Data.FileList[1]) == "text11.txt");
+            Assert.IsTrue(System.IO.Path.GetFileName(explorer.Data.FileList[2]) == "text12.txt");
+        }
+
+        private dynamic GetPrivateField(Explorer explorer, string fieldName)
+        {
+            PrivateObject po = new PrivateObject(explorer);
+            return po.GetField(fieldName);
+        }
+    }
+}
