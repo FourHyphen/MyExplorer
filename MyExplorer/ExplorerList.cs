683b05a5c10382aafa6838359b08436547751f52
FourHyphen <FourHyphen.4@gmail.com>
2021-07-27 08:06:32 +0900

    ExplorerListクラスのインスタンス生成テストを追加

---
+++ MyExplorer/ExplorerList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExplorer
+{
+    public class ExplorerList
+    {
+        private List<Explorer> Explorers { get; set; } = new List<Explorer>();
+
+        public void Add(string folderPath)
+        {
+            Explorer explorer = new Explorer(folderPath, Explorers.Count);
+            Explorers.Add(explorer);
+        }
+    }
+}
--- TestMyExplorer/TestExplorer.cs
+++ TestMyExplorer/TestExplorer.cs
@@ -29,19 +29,34 @@ namespace TestMyExplorer
             Explorer explorer = new Explorer(folderPath, 0);
             Assert.IsTrue(explorer.Data.FolderPath.Contains(@"TestData\Folder1"));
 
-            Assert.IsTrue(GetPrivateField(explorer, "FolderArea").Name == "FolderArea0");
-            Assert.IsTrue(GetPrivateField(explorer, "Folder").Name == "Folder0");
-            Assert.IsTrue(GetPrivateField(explorer, "FolderFileList").Name == "FileList0");
+            Assert.IsTrue(GetPrivateMember(explorer, "FolderArea").Name == "FolderArea0");
+            Assert.IsTrue(GetPrivateMember(explorer, "Folder").Name == "Folder0");
+            Assert.IsTrue(GetPrivateMember(explorer, "FolderFileList").Name == "FileList0");
 
             Assert.IsTrue(System.IO.Path.GetFileName(explorer.Data.FileList[0]) == "folder01");
             Assert.IsTrue(System.IO.Path.GetFileName(explorer.Data.FileList[1]) == "text11.txt");
             Assert.IsTrue(System.IO.Path.GetFileName(explorer.Data.FileList[2]) == "text12.txt");
         }
 
-        private dynamic GetPrivateField(Explorer explorer, string fieldName)
+        [TestMethod]
+        public void CreateExplorerList()
         {
-            PrivateObject po = new PrivateObject(explorer);
-            return po.GetField(fieldName);
+            string folderPath0 = Common.GetFilePathOfDependentEnvironment(@"./TestData/Folder1");
+            string folderPath1 = Common.GetFilePathOfDependentEnvironment(@"./TestData/Folder2");
+
+            ExplorerList explorerList = new ExplorerList();
+            explorerList.Add(folderPath0);
+            explorerList.Add(folderPath1);
+
+            dynamic explorers = GetPrivateMember(explorerList, "Explorers");
+            Assert.IsTrue(explorers[0].Data.FolderPath.Contains(@"TestData\Folder1"));
+            Assert.IsTrue(explorers[1].Data.FolderPath.Contains(@"TestData\Folder2"));
+        }
+
+        private dynamic GetPrivateMember(dynamic instance, string memberName)
+        {
+            PrivateObject po = new PrivateObject(instance);
+            return po.GetFieldOrProperty(memberName);
         }
     }
 }
