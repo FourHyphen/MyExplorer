90feb2566032e52da75b53185a7296193cd4dde0
FourHyphen <FourHyphen.4@gmail.com>
2021-09-12 15:13:42 +0900

    リファクタ: ExplorerのFileList系処理を別ファイルにPartial化

--- MyExplorer/Explorer.xaml.cs
+++ MyExplorer/Explorer.xaml.cs
@@ -96,55 +96,6 @@ namespace MyExplorer
             PropertyChanged?.Invoke(this, e);
         }
 
-        public bool IsFocusedItemInFileList()
-        {
-            ListViewItem item = GetListViewItemFocused();
-            return item != null;
-        }
-
-        public ListViewItem GetListViewItemFocused()
-        {
-            // 参考: https://threeshark3.com/binding-listbox-focus/
-            for (int i = 0; i < FolderFileList.Items.Count; i++)
-            {
-                var obj = GetItemInFolderFileList(i);
-                if (obj is ListViewItem target)
-                {
-                    if (target.IsFocused)
-                    {
-                        return target;
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private object GetItemInFolderFileList(int index)
-        {
-            return FolderFileList.ItemContainerGenerator.ContainerFromIndex(index);
-        }
-
-        public ListViewItem GetListViewItem(string content)
-        {
-            for (int i = 0; i < FolderFileList.Items.Count; i++)
-            {
-                var obj = GetItemInFolderFileList(i);
-                if (obj is ListViewItem target)
-                {
-                    if (target.Content is ExplorerFileInfo efi)
-                    {
-                        if (efi.Name == content)
-                        {
-                            return target;
-                        }
-                    }
-                }
-            }
-
-            return null;
-        }
-
         /// <summary>
         /// テストから直接呼び出す
         /// </summary>
